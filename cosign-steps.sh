74f36971d4b4165f71a415be3f518ae3069d20ba
damian0519 <damian_0519@o2.pl>
2024-05-31 12:25:17 +0200

    Update cosign-steps.sh

--- cosign-steps.sh
+++ cosign-steps.sh
@@ -1,10 +1,10 @@
-export COSIGN_PASSWORD="C@S1GN#$&"
+export COSIGN_PASSWORD="damian"
 
 # Sign the docker image
-cosign sign --key cosign.key arthanaharidas/python-hello-world
+cosign sign --key cosign.key damian0519/python-hello-world
 
 # Verify the docker image
-cosign verify --key cosign.pub arthanaharidas/python-hello-world
+cosign verify --key cosign.pub damian0519/python-hello-world
 
 # Logout docker
 docker logout
